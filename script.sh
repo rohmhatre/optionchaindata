00c0e11da5b815840cf63656a17c912de2d98597
rohmhatre <roh.mhatre@gmail.com>
2018-12-26 11:57:51 +0530

    Commit on 2018-12-2611-12

--- script.sh
+++ script.sh
@@ -2,3 +2,9 @@
 cd /Sites/option_chain
 source /Sites/env/bin/activate
 python optionchain.py
+DATE=`date +%Y-%m-%d%H-%m`
+ls -ltr /Sites/option_chain/NIFTY |awk '{if (NR!=1){print $9}}'> /Sites/option_chain/niftyoptionlist.txt
+COMMIT_MSG="Commit on "$DATE
+git add	.
+git commit -m "$COMMIT_MSG"
+git push -u origin master
