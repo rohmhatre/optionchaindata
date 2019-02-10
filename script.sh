04f4180a1ca40374e4f02c728a530db0e5047381
rohmhatre <roh.mhatre@gmail.com>
2019-02-10 23:20:02 +0530

    Commit on 2019-02-10_23-02

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@ do
    j=$(echo "$i" | awk '{print tolower($0)}');
    ls -ltr /Sites/option_chain/$i |awk '{if (NR!=1){print $9}}'>/Sites/option_chain/lists/$j.txt; 
 done
-python create_analysis_csv.py
+python create_analysis.py
 python SendMail.py 
 COMMIT_MSG="Commit on "$DATE
 git add	.
